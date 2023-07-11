ffe5167e0f16d3cead6b5b7c796712d8b77a27d1
chenjshnn <44050770+chenjshnn@users.noreply.github.com>
2023-07-11 16:54:11 +1000

    Update Demo_control.vue

--- src/components/Demo_control.vue
+++ src/components/Demo_control.vue
@@ -59,9 +59,9 @@ export default {
         },
        
         {
-          title: 'https://nvd.nist.gov/vuln/detail/CVE-2021-21841',
-          recommendated: "https://github.com/gpac/gpac/tree/70607fc71a671cf48a05e013a4e411429373dce7",
-          recommendatedFiles: ['https://github.com/gpac/gpac/blob/70607fc71a671cf48a05e013a4e411429373dce7/src/isomedia/box_code_base.c'],
+          title: 'https://nvd.nist.gov/vuln/detail/CVE-2014-3569',
+          recommendated: "https://github.com/openssl/openssl/tree/5ad4fdce41bb1ce7762b70fb50f732f70e3772cf",
+          recommendatedFiles: ['https://github.com/openssl/openssl/blob/5ad4fdce41bb1ce7762b70fb50f732f70e3772cf/ssl/s3_srvr.c'],
         },
         // {
         //   title: 'https://nvd.nist.gov/vuln/detail/',
